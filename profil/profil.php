cf095f978c6f20a98b57234f6dd86c1c7b43f044
Jules <ju.pabst@gmail.com>
2023-12-15 15:19:19 +0100

    fixed profil bearbeiten bugs

--- profil/profil.php
+++ profil/profil.php
@@ -1,7 +1,11 @@
 <?php
-if((isset($_POST["passwortLoggedIn"])) && $_POST["passwortLoggedIn"] == $_SESSION["passwortLoggedIn"] ){
-    include 'profilBearbeiten.php';
-} else {
-    include 'profilAnzeigen.php';
-}
+  if(isset($_POST["passwortLoggedIn"]) && $_POST["passwortLoggedIn"] == $_SESSION["passwortLoggedIn"] && !isset($_POST["anrede"])) {
+    $_SESSION["passwortCheckProfil"] = 1;
+    include 'profil/profilBearbeiten.php';
+  } else {
+    if(isset($_POST["passwortLoggedIn"])){
+        $_SESSION["passwortCheckProfil"] = 0;
+    }
+    include 'profil/profilAnzeigen.php';
+  }
 ?>
--- profil/profilAnzeigen.php
+++ profil/profilAnzeigen.php
@@ -6,8 +6,7 @@
 <p class="listen">E-Mail: <?php echo $_SESSION["Email"]?></p>
 <p class="listen">Username: <?php echo $_SESSION["usernameLoggedIn"]?></p>
 
-
-<form action="index.php?site=profilBearbeiten" method="post">
+<form action="index.php?site=profil" method="post">
     <input name="bearbeiten" value="ja" hidden>
     <button type= submit>Profil bearbeiten</button>
 </form>
@@ -16,12 +15,16 @@
 if(isset($_POST["bearbeiten"])){
     echo '
     <form action="index.php?site=profil" method="post">
-        <div>
-        <label for="passwortLoggedIn">Passwort:</label>
+        <div>';
+    if(isset($_SESSION["passwortCheckProfil"]) && $_SESSION["passwortCheckProfil"] == 0){
+        echo "<div><label style='font-weight: 600;'>Falsches Passwort!</label></div>";
+    }
+    echo '<label for="passwortLoggedIn">Passwort:</label>
         <input 
         type="password"
         name="passwortLoggedIn"
         />
+        <input name="bearbeiten" value="ja" hidden>
         </div>
         <button type="submit">Senden</button>
     </form>';
